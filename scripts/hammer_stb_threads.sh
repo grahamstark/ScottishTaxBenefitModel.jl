f78e0440312b1386edaa7ef1ce2c3b4c1a5b92fd
Graham Stark <graham.stark@virtual-worlds.biz>
2020-06-02 21:43:58 +0100

    renaming...

---
+++ scripts/hammer_stb_threads.sh
@@ -0,0 +1,33 @@
+## Crude script to hammer the stb server with 16 threads, each of 16 requests
+cd o1
+time sh ../hammer_stb.sh &
+cd ../o2
+time sh ../hammer_stb.sh &
+cd ../o3
+time sh ../hammer_stb.sh &
+cd ../o4
+time sh ../hammer_stb.sh &
+cd ../o5
+time sh ../hammer_stb.sh &
+cd ../o6
+time sh ../hammer_stb.sh &
+cd ../o7
+time sh ../hammer_stb.sh &
+cd ../o8
+time sh ../hammer_stb.sh &
+cd ../o9
+time sh ../hammer_stb.sh &
+cd ../o10
+time sh ../hammer_stb.sh &
+cd ../o11
+time sh ../hammer_stb.sh &
+cd ../o12
+time sh ../hammer_stb.sh &
+cd ../o13
+time sh ../hammer_stb.sh &
+cd ../o14
+time sh ../hammer_stb.sh &
+cd ../o15
+time sh ../hammer_stb.sh &
+cd ../o16
+time sh ../hammer_stb.sh &
