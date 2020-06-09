a3fddef74c0588d35200d36a2615c777da41e65d
Graham Stark <graham.stark@virtual-worlds.biz>
2020-06-09 10:36:03 +0100

    messing about trying to get Travis to work..

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using ScottishTaxBenefitModel
 using Test
 
+##  for Travis - must be a better way ...
+if ! ( "src/" in LOAD_PATH )
+    push!( LOAD_PATH, "src/")
+end
+
 include( "income_tax_tests.jl")
 include( "parameter_test.jl")
 include( "household_tests.jl")
