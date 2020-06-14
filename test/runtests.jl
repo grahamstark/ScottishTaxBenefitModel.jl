fd7ab28017ba753676639993a7d44125715af0bf
Graham Stark <graham.stark@virtual-worlds.biz>
2020-06-14 21:22:36 +0100

    conditional running of main dataset, till I've made randomised version.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,15 @@
 using Test
 using ScottishTaxBenefitModel
 
+is_local = ENV['JULIA_IS_LOCALLY_INSTALLED'] == 1 # fixme param
+
 include( "income_tax_tests.jl")
 include( "parameter_tests.jl")
-include( "household_tests.jl")
-include( "simple_runner_tests.jl")
+if is_local
+# This will only run locally
+    include( "household_tests.jl")
+    include( "simple_runner_tests.jl")
+end
 
 @testset "ScottishTaxBenefitModel.jl" begin
     # Write your tests here.
