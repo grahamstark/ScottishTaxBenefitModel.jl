9f734eaaaebadc9f23f8df5910e93f8e21088fdf
Graham Stark <graham.stark@virtual-worlds.biz>
2020-07-02 18:51:18 +0100

    cleanup tests a wee bit.

--- test/legacy_mt_tests.jl
+++ test/legacy_mt_tests.jl
@@ -13,19 +13,17 @@ const RUK_PERSON = 100000001001
 
 @testset "CPAG" begin
     # BASIC IT Calcaulation on
-    nisys = NationalInsuranceSys()
-    weeklyise!( nisys )
+
     @time names = ExampleHouseholdGetter.initialise()
     income = [110.0,145.0,325,755.0,1_000.0]
 
     ntests = size(income)[1]
-    @test ntests == size( nidue )[1]
     hh = ExampleHouseholdGetter.get_household( "mel_c2" )
     pers = hh.people[RUK_PERSON]
     pers.age = 50
     for i in 1:ntests
         pers.income[wages] = income[i]
-        
+
 
     end
 end # t1
