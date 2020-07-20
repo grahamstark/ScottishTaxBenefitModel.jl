800ff5c0dbeebcd7c52586c0678fbdf9b755bd29
Graham Stark <graham.stark@virtual-worlds.biz>
2020-07-20 16:55:11 +0100

    typing results thing.

--- src/Results.jl
+++ src/Results.jl
@@ -11,9 +11,11 @@ module Results
     using .IncomeTaxCalculations: ITResult
     using .NationalInsuranceCalculations: NIResult
 
-    @with_kw mutable struct individual_results
-       ni = NIResult()
-       it = ITResult()
+    export IndividualResults, BenefitUnitResults,HouseholdResults
+
+    @with_kw mutable struct IndividualResults{IT<:Integer, RT<:Real}
+       ni = NIResult{IT,RT}()
+       it = ITResult{IT,RT}()
        # ...
     end
 
@@ -29,42 +31,43 @@ module Results
         make_individual_results_frame( Float64, n )
     end
 
-    function make_individual_results_frame( NR :: DataType=Float64; n :: Int ) :: DataFrame
+    function make_individual_results_frame( RT :: DataType, n :: Int ) :: DataFrame
        DataFrame(
          pid = zeros(BIGINT,n),
-         weight = zeros(NR,n),
+         weight = zeros(RT,n),
          sex = zeros(Int,n),
          age_band  = zeros(Int,n),
          employment = zeros(Int,n),
 
-         gross_income = zeros(NR,n),
-         net_income  = zeros(NR,n)
-         total_taxes = zeros(NR,n),
-         total_benefits = zeros(NR,n),
-         income_tax = zeros(NR,n),
-         it_non_savings = zeros(NR,n),
-         it_savings = zeros(NR,n),
-         it_dividends = zeros(NR,n),
+         gross_income = zeros(RT,n),
+         net_income  = zeros(RT,n)
+         total_taxes = zeros(RT,n),
+         total_benefits = zeros(RT,n),
+         income_tax = zeros(RT,n),
+
+         it_non_savings = zeros(RT,n),
+         it_savings = zeros(RT,n),
+         it_dividends = zeros(RT,n),
 
          ni_above_lower_earnings_limit = fill( false, n )
-         ni_total_ni = zeros(NR,n)
-         ni_class_1_primary = zeros(NR,n)
-         ni_class_1_secondary = zeros(NR,n)
-         ni_class_2  = zeros(NR,n)
-         ni_class_3  = zeros(NR,n)
-         ni_class_4  = zeros(NR,n)
-         assumed_gross_wage = zeros(NR,n)
+         ni_total_ni = zeros(RT,n)
+         ni_class_1_primary = zeros(RT,n)
+         ni_class_1_secondary = zeros(RT,n)
+         ni_class_2  = zeros(RT,n)
+         ni_class_3  = zeros(RT,n)
+         ni_class_4  = zeros(RT,n)
+         assumed_gross_wage = zeros(RT,n)
 
 
-         benefit1 = zeros(NR,n),
-         benefit2 = zeros(NR,n),
-         basic_income = zeros(NR,n),
-         net_income = zeros(NR,n),
-         metr = zeros(NR,n),
-         tax_credit = zeros(NR,n),
-         vat = zeros(NR,n),
-         other_indirect = zeros(NR,n),
-         total_indirect = zeros(NR,n))
+         benefit1 = zeros(RT,n),
+         benefit2 = zeros(RT,n),
+         basic_income = zeros(RT,n),
+         net_income = zeros(RT,n),
+         metr = zeros(RT,n),
+         tax_credit = zeros(RT,n),
+         vat = zeros(RT,n),
+         other_indirect = zeros(RT,n),
+         total_indirect = zeros(RT,n))
     end
 
 
