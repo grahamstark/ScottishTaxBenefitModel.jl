71aab2224338222734cc4fbac903becb55c3c374
Graham Stark <graham.stark@virtual-worlds.biz>
2020-08-05 08:12:38 +0100

    test suite for any odd functions lying around.

---
+++ test/general_tests.jl
@@ -0,0 +1,15 @@
+using Test
+using ScottishTaxBenefitModel
+using .Utils
+
+@enum A a b c d e
+
+@testset "tests of miscellaneous functions" begin
+
+    gr = Dict{A,Real}( a=>1.0, b=>2, e=> 4.1 )
+    ne = Dict{A,Real}( c=>2, b=>9 )
+    inc = Dict{A,Real}( a=>1, b=>1.0, c=>1, d =>1 )
+    m = mult( gross=gr, net=ne, incl=inc )
+    @test m == 12
+
+end
