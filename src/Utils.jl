6f5265b7c154bb0b1b5ef74633862c3453c68192
Graham Stark <graham.stark@virtual-worlds.biz>
2020-10-19 18:01:32 +0100

    4 pages of examples now..

--- src/Utils.jl
+++ src/Utils.jl
@@ -21,6 +21,13 @@ function eq_nearest_p( a :: Real, b :: Real )
    round( a, digits=2) == round( b, digits=2 )
 end
 
+"""
+For Unit testing
+"""
+function get_birthdate( age_in_years :: Integer, from_date :: DateTime = now() ) :: Date
+    Date(from_date - Year( age_in_years ) - Month(1))
+end
+
 @generated function operate_on_struct!( rec::Rec, x::NR, f::Function ) where Rec where NR<:Number
      assignments = [
          :( rec.$name = f(rec.$name, x) ) for name in fieldnames(Rec)
