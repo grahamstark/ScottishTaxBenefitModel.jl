312239742c5b1288c248632495eccf31476c316c
Graham Stark <graham.stark@virtual-worlds.biz>
2020-12-11 14:54:17 +0000

    minor test changes

--- test/simple_runner_tests.jl
+++ test/simple_runner_tests.jl
@@ -18,46 +18,31 @@ settings = RunSettings()
 BenchmarkTools.DEFAULT_PARAMETERS.seconds = 120
 BenchmarkTools.DEFAULT_PARAMETERS.samples = 2
 
-sys = [get_system(scotland=false), get_system( scotland=true )]
 
-function basic_run( )
+function basic_run( ; print_test :: Bool )
 
-    global print_test
-
+    sys = [get_system(scotland=false), get_system( scotland=true )]
     results = do_one_run!( settings, sys )
 
     #
-    #  = summarise_results!( results=results, base_results=base_results )
     #
-    # if print_test
-    #     print( "   deciles = $( summary_output.deciles)\n\n" )
-    #
-    #     print( "   poverty_line = $(summary_output.poverty_line)\n\n" )
-    #
-    #     print( "   inequality = $(summary_output.inequality)\n\n" )
-    #
-    #     print( "   poverty = $(summary_output.poverty)\n\n" )
-    #
-    #     print( "   gainlose_by_sex = $(summary_output.gainlose_by_sex)\n\n" )
-    #     print( "   gainlose_by_thing = $(summary_output.gainlose_by_thing)\n\n" )
-    #
-    #     print( "   metr_histogram= $(summary_output.metr_histogram)\n\n")
-    #     println( "SUMMARY OUTPUT")
-    #     println( summary_output )
-    #     println( "as JSON")
-    #     println( JSON.json( summary_output ))
-    # end
-
-end # summary_output.timing summary_output.blockt = JSON.json( summary_output )
-
-# example_names, total_num_households, total_num_people = load_data( load_examples = true, load_main = true, start_year = 2015 )
-#
-# params = deepcopy(ScottishTaxBenefitModel.MiniTB.DEFAULT_PARAMS)
-#
-# base_results = create_base_results( total_num_households, total_num_people )
+    if print_test
+         summary_output = summarise_results!( results=results, base_results=base_results )
+         print( "   deciles = $( summary_output.deciles)\n\n" )
+         print( "   poverty_line = $(summary_output.poverty_line)\n\n" )
+         print( "   inequality = $(summary_output.inequality)\n\n" )        
+         print( "   poverty = $(summary_output.poverty)\n\n" )
+         print( "   gainlose_by_sex = $(summary_output.gainlose_by_sex)\n\n" )
+         print( "   gainlose_by_thing = $(summary_output.gainlose_by_thing)\n\n" )
+         print( "   metr_histogram= $(summary_output.metr_histogram)\n\n")
+         println( "SUMMARY OUTPUT")
+         println( summary_output )
+         println( "as JSON")
+         println( JSON.json( summary_output ))
+    end
+end 
 
 @testset "basic run timing" begin
-    t = @benchmark basic_run()
-
+    t = @benchmark basic_run( print_test=false )
     print(t)
 end
