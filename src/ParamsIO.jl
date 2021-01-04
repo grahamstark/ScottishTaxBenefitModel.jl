046edad14299eecd9afb8dcbb3d59f1c81a20dbf
Graham Stark <graham.stark@virtual-worlds.biz>
2021-01-04 23:05:57 +0000

    comments

--- src/ParamsIO.jl
+++ src/ParamsIO.jl
@@ -1,3 +1,7 @@
+# THIS DOESN'T WORK !!!1
+# json3 turns the structures below the top-level into Dicts
+# and I don't know why.
+# Wasted 2 days on this ...
 #
 # See scripts/serialisation_experiments.jl this one always works but has an ugly global variable
 # I'm using this for now.
