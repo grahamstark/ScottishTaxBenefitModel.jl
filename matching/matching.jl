e74daedfb1d00cbe7414972d47502a71adb62d31
grahamstark <graham.stark@virtual-worlds.biz>
2021-02-08 12:12:06 +0000

    frs shs matching scripts

--- matching/matching.jl
+++ matching/matching.jl
@@ -8,59 +8,9 @@ using .Utils: coarse_match
 # * match routine `` in [.Utils](https://github.com/grahamstark/ScottishTaxBenefitModel.jl/blob/master/src/Utils.jl)
 # * [blog post](https://stb-blog.virtual-worlds.scot/articles/2021/02/07/matching-notes.html) (includes some references);
 # * test cases [matching_tests.jl]()https://github.com/grahamstark/ScottishTaxBenefitModel.jl/blob/master/test/matching_tests.jl
-# 
-#
-# load all 3 shs social datasets
 #
 const DIR="/mnt/transcend/data/"
 const SCOTLAND=299999999
-
-function loadshs( year::Int )::DataFrame
-	year -= 2000
-	ystr = "$(year)$(year+1)"
-	fname = "$(DIR)/shs/$(ystr)/tab/shs20$(year)_social_public.tab"
-	println( "loading '$fname'" )
-	shs = CSV.File( fname; missingstrings=["NA"] ) |> DataFrame
-	lcnames = Symbol.(lowercase.(string.(names(shs))))
-    names!(shs,lcnames)
-    shs.datayear = year
-    return shs
-end
-
-
-function loadfrs( year::Int, fname :: String ) :: DataFrame
-	ystr = "$(year)$(year+1)"
-	fname = "$(DIR)/frs/$(year)/tab/$(fname).tab"
-	println( "loading '$fname'" )
-	frs = CSV.File( fname; missingstrings=["-1"] ) |> DataFrame
-	lcnames = Symbol.(lowercase.(string.(names(frs))))
-    names!(frs,lcnames)
-    frs.datayear = year
-    return frs
-end
-
-
-s16=loadshs(2016)
-s17=loadshs(2017)
-s18=loadshs(2018)
-
-hh16 = loadfrs( 2016, "househol" )
-hh17 = loadfrs( 2017, "househol" )
-hh18 = loadfrs( 2018, "househol" )
-
-shh16 = hh16[(hh16.gvtregn .== SCOTLAND),:]
-shh17 = hh17[(hh17.gvtregn .== SCOTLAND),:]
-shh18 = hh18[(hh18.gvtregn .== SCOTLAND),:]
-
-
-ad16 = loadfrs( 2016, "adult" )
-ad17 = loadfrs( 2017, "adult" )
-ad18 = loadfrs( 2018, "adult" )
-
-shhad16=innerjoin( ad16,shh16, on=:sernum; makeunique=true )
-shhad17=innerjoin( ad17,shh17, on=:sernum; makeunique=true )
-shhad18=innerjoin( ad18,shh18, on=:sernum; makeunique=true )
-
 
 function make_highest_hh_income_marker( hhad :: DataFrame ) :: Vector{Bool}
 	sns = unique(hhad.sernum)
@@ -92,18 +42,95 @@ function make_highest_hh_income_marker( hhad :: DataFrame ) :: Vector{Bool}
 	return highest_income
 end
 
+function loadshs( year::Int )::DataFrame
+	year -= 2000
+	ystr = "$(year)$(year+1)"
+	fname = "$(DIR)/shs/$(ystr)/tab/shs20$(year)_social_public.tab"
+	println( "loading '$fname'" )
+	shs = CSV.File( fname; missingstrings=["NA"] ) |> DataFrame
+	lcnames = Symbol.(lowercase.(string.(names(shs))))
+    names!(shs,lcnames)
+    shs.datayear = year
+    return shs
+end
+
+
+function loadfrs( year::Int, fname :: String ) :: DataFrame
+	ystr = "$(year)$(year+1)"
+	fname = "$(DIR)/frs/$(year)/tab/$(fname).tab"
+	println( "loading '$fname'" )
+	frs = CSV.File( fname; missingstrings=["-1"] ) |> DataFrame
+	lcnames = Symbol.(lowercase.(string.(names(frs))))
+    names!(frs,lcnames)
+    frs.datayear = year
+    return frs
+end
+
+#
+# Load all 3 shs social datasets
+#
+s16=loadshs(2016)
+s17=loadshs(2017)
+s18=loadshs(2018)
+
+#
+# Load 3 FRS hhld datasets, and then make Scotland only subsets.
+#
+hh16 = loadfrs( 2016, "househol" )
+hh17 = loadfrs( 2017, "househol" )
+hh18 = loadfrs( 2018, "househol" )
+
+shh16 = hh16[(hh16.gvtregn .== SCOTLAND),:]
+shh17 = hh17[(hh17.gvtregn .== SCOTLAND),:]
+shh18 = hh18[(hh18.gvtregn .== SCOTLAND),:]
+
+#
+# FRS Adult datasets.
+#
+ad16 = loadfrs( 2016, "adult" )
+ad17 = loadfrs( 2017, "adult" )
+ad18 = loadfrs( 2018, "adult" )
+
+#
+# joined household and adult FRS records, Scotland only
+#
+shhad16=innerjoin( ad16,shh16, on=:sernum; makeunique=true )
+shhad17=innerjoin( ad17,shh17, on=:sernum; makeunique=true )
+shhad18=innerjoin( ad18,shh18, on=:sernum; makeunique=true )
+
+#
+# now, delete all but the adult with the highest income, since this is  
+# 
 shhad16.highest_income = make_highest_hh_income_marker( shhad16 )
 shhad17.highest_income = make_highest_hh_income_marker( shhad17 )
 shhad18.highest_income = make_highest_hh_income_marker( shhad18 )
 
+# some dumps, just for curiousity
 shhad18[:,[:sernum,:person,:indinc,:highest_income]] 
-# mbu members 2nd+ bus
+# check for mbu members 2nd+ bus
 shhad18[((shhad18.highest_income.==1) .& (shhad18.person.>2)),[:sernum,:person,:indinc,:highest_income]]
 
-
-shh_ad_highest_comm = vcat(
+#
+# Stack all 3 years FRS.
+# 
+frs_all_years_scot_he = vcat(
 	shhad16[shhad16.highest_income,:],
 	shhad17[shhad17.highest_income,:],
 	shhad18[shhad18.highest_income,:];
 	cols=:intersect)
                       
+# ... 
+# Stack shs.
+# 
+shs_all_years = vcat(
+    s16,
+    s17,
+    s18,
+    cols=:intersect
+)
+
+#
+# Create donor  (SHS) and recipient (FRS) datasets.
+#
+donor_ds = DataFrame() # shs
+recipient = DataFrame() # frs
