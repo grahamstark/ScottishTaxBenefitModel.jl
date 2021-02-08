d593cfa7ea02a122272469e29a9f9b6d323a387c
grahamstark <graham.stark@virtual-worlds.biz>
2021-02-08 11:54:53 +0000

    basic matching thing works

--- matching/matching.jl
+++ matching/matching.jl
@@ -1,49 +1,19 @@
 using CSV,DataFrames
+using ScottishTaxBenefitModel
+using .Utils: coarse_match
+#
+# Scripts for creating a merged FRS (recipient) and SHS (donor) datase
+# see the following:
+# 
+# * match routine `` in [.Utils](https://github.com/grahamstark/ScottishTaxBenefitModel.jl/blob/master/src/Utils.jl)
+# * [blog post](https://stb-blog.virtual-worlds.scot/articles/2021/02/07/matching-notes.html) (includes some references);
+# * test cases [matching_tests.jl]()https://github.com/grahamstark/ScottishTaxBenefitModel.jl/blob/master/test/matching_tests.jl
+# 
 #
 # load all 3 shs social datasets
 #
 const DIR="/mnt/transcend/data/"
-
-function coarse_match( 
-	recip :: DataFrameRow, 
-	donor :: DataFrame, 
-	vars  :: Vector{Symbol},
-	max_matches :: Int,
-	max_coarsens :: Int ) :: NamedTuple
-	nobs = size( donor )[1]
-	nvars = size( vars )[1]
-	c_level = ones(Int,nvars)
-	qualities = zeros(Int,nobs)
-	quality = 0
-	prevmatches = fill( false, nobs )
-	
-	matches = fill( true, nobs )
-	for nc in 2:max_coarsens
-		for nv in 1:nvars
-			matches = fill( true, nobs )
-			for n in 1:nvars
-				# so, if sym[1] = :a and c_level[1] = 1 then :a_1 and so on
-				sym = Symbol("$(String(vars[n]))_$(c_level[n])") # everything
-				println( "n = $n using sym $(sym)" )
-				matches .&= (donor[sym] .== r1[sym])			
-			end
-			c_level[nv] = nc
-			println( "c_level now $(c_level) matches $(matches)" )
-			nmatches = sum( matches )
-			println( "nmatches $nmatches max_matches $max_matches quality $quality" )
-			quality += 1
-			newmatches = prevmatches .⊻ matches
-			println( "newmatches $(newmatches) prevmatches=$prevmatches" )
-			qualities[newmatches] .= quality
-			prevmatches = matches
-			println( "end of loop" )
-			if nmatches >= max_matches
-				return (matches=matches,qualities=qualities)
-			end
-		end # vars
-	end # coarse
-	return (matches=matches,qualities=qualities)
-end
+const SCOTLAND=299999999
 
 function loadshs( year::Int )::DataFrame
 	year -= 2000
@@ -78,9 +48,9 @@ hh16 = loadfrs( 2016, "househol" )
 hh17 = loadfrs( 2017, "househol" )
 hh18 = loadfrs( 2018, "househol" )
 
-shh16 = hh16[(hh16.gvtregn .== 299999999),:]
-shh17 = hh17[(hh17.gvtregn .== 299999999),:]
-shh18 = hh18[(hh18.gvtregn .== 299999999),:]
+shh16 = hh16[(hh16.gvtregn .== SCOTLAND),:]
+shh17 = hh17[(hh17.gvtregn .== SCOTLAND),:]
+shh18 = hh18[(hh18.gvtregn .== SCOTLAND),:]
 
 
 ad16 = loadfrs( 2016, "adult" )
@@ -137,36 +107,3 @@ shh_ad_highest_comm = vcat(
 	shhad18[shhad18.highest_income,:];
 	cols=:intersect)
                       
-
-
-# CSV.write( "$(DIR)/shs/merged_soc/shs_16_17_18_merged_common_vars.tab", hhad ) 
-# s1618all = vcat(s16,s17,s18;cols=:union)
-# CSV.write( "$(DIR)/shs/merged_soc/s16_17_18_merged_all_vars.tab", s1618all )
-
-donor = DataFrame( sernum=[1,2,3,4], a_1=[1,2,3,4], a_2=[8,2,3,1], b_1=[1,2,3,4], b_2=[6,7,7,8] )
-
-n = 10000
-donor = DataFrame( sernum=collect(1:n), a_1=rand(1:50,n), b_1=rand(100:1500,n))
-# coarsend
-donor.a_2 = donor.a_1 .<= 25
-donor.b_2 = donor.b_1 .<= 600
-
-m = 5000
-recip = DataFrame( sernum=collect(1:m), a_1=rand(2:500,m), b_1=rand(11:17,m))
-# coarsend
-recip.a_2 = recip.a_1 .<= 230
-recip.b_2 = recip.b_1 .<= 700
-
-r1 = recip[1,:]
-
-matches = coarse_match( 
-	r1,
-	donor,
-	[:a, :b],
-	10,
-	25 )
-end
-
-
-matches = donor.a_1 .== r1.a_1
-matches .&= (donor[:b_1] .== r1[:b_1])
