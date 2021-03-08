3e88d24cad641d35d1d914b0f8dc83a1711d7bd1
Graham Stark <graham.stark@virtual-worlds.biz>
2021-03-08 22:33:03 +0000

    rooms calcs

--- src/LocalLevelCalculations.jl
+++ src/LocalLevelCalculations.jl
@@ -15,7 +15,103 @@ using .STBParameters: LegacyMeansTestedBenefitSystem, IncomeRules,
     
 using .GeneralTaxComponents: TaxResult, calctaxdue, RateBands
 
+    #
+    # local stuff for numbers of rooms. A hack, almost certainly 
+    # not completely right, but near enough I hope
+    # cpag ch6 p94-
+    # Smallest number of rooms for U15s with the following rules:
+    # 1. any u10s can share
+    # 2. 10+s of same sex can share
+    #
+    struct P
+        sex :: Sex
+        age :: Int
+        pid :: BigInt
+    end
     
+    function match( p1::P, p2::P )::Bool
+        if p1.age < 10 && p2.age < 10
+            return true
+        elseif p1.sex == p2.sex 
+            return true
+        end
+        return false
+    end
+    
+    function pairup!( prs :: Array, unpaired :: Array{P} )
+        n = size( unpaired )[1]
+        # println( "initial unpaired = $(unpaired) n = $n " )
+        # println( "initial prs $prs" )
+        if n == 1
+            push!( prs, unpaired[1] )  
+            deleteat!( unpaired, 1 )
+            return
+        elseif n == 0
+            return
+        elseif (n == 2) && !( match( unpaired[1], unpaired[2] ))
+            push!( prs, unpaired[1] )  
+            deleteat!( unpaired, 1 )
+            push!( prs, unpaired[1] )  
+            deleteat!( unpaired, 1 )    
+            return
+        end
+        found = false
+        for i in 2:n
+            p1 = unpaired[1]
+            p2 = unpaired[i]
+            if match( p1, p2 )
+                # println( "matched 1 and $i " )
+                push!( prs, [ p1, p2 ])
+                # println( "prs now $prs ")
+                deleteat!( unpaired, 1 )
+                deleteat!( unpaired, i-1 )
+                # println( "unpaired now $(unpaired)" )
+                found = true
+                break
+           end
+        end
+        if ! found 
+            push!( prs, unpaired[1])
+            deleteat!( unpaired, 1 )
+        end
+        pairup!( prs, unpaired )    
+    end
+    
+    function swap!(a::Vector,p1,p2::Int )
+        tmp = a[p1]
+        a[p1] = a[p2]
+        a[p2] = tmp
+    end
+    
+    """
+    Smallest number of rooms for U15s with the following rules:
+    1. any u10s can share
+    2. 10+s of same sex can share
+    FIXME This is NOT COMPLETELY RIGHT and very hacky but might be close enough
+    Find someone who knows combinatorics....
+    """
+    function min_kids_rooms( children :: Vector{P} ) :: Int
+        if n == 0
+            return 0
+        end
+        mr = 9999
+        n = size( children )[1]    
+        for i in 2:n
+            prs = []
+            un = copy(children)
+            swap!(un, 2, i )
+            pairup!( prs, un )
+            sz = size( prs )[1]
+            mr = min( mr, sz )
+            # println(sz)
+            # println( prs )
+        end
+        mr
+    end    
+    
+    function num_rooms( hh :: Household, 
+
+    end
 
 	export calc_lha, calc_bedroom_tax, calc_council_tax, initialise
 
