9f6e517261c91b4837d95d9907714a3f5a73ce1b
Graham Stark <graham.stark@virtual-worlds.biz>
2021-06-10 15:09:48 +0100

    code snippets

---
+++ scripts/code_snippets.jl
@@ -0,0 +1,7 @@
+# Snippets
+
+# dist of everyone on a benefit
+countmap(people.income_widows_payment)
+
+# extract some info on everyone with some benefit 
+people[((people.income_bereavement_allowance_or_widowed_parents_allowance_or_bereavement.>0).& .! ismissing.(people.income_bereavement_allowance_or_widowed_parents_allowance_or_bereavement)),[:age,:sex,:income_bereavement_allowance_or_widowed_parents_allowance_or_bereavement,:data_year]]
