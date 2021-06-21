6bc69a005cd736119317230f7e7310a301730969
Graham Stark <graham.stark@virtual-worlds.biz>
2021-06-21 04:54:07 +0100

    back to nmt bens

--- src/HistoricBenefits.jl
+++ src/HistoricBenefits.jl
@@ -53,7 +53,7 @@ function get_historic( fy :: Integer, which :: Symbol, width::Int = 1 )::Vector{
 end
 
 """
-  Find an exact match for v -1..+1 years around fy, or find 
+  Find an exact match for some benefit level, given actual levels, -1..+1 years around fy, or find 
   the nearest amongst the current values, since sometimes the imputation
   seems to be for the wrong year (calendar vs financial). If that fails, pick the
   index of the current values that's closest.
@@ -115,56 +115,4 @@ function make_benefit_ratios( fy :: Integer, incd :: Incomes_Dict{T} ) ::Incomes
     return d
 end
     
-    #=
-        # sometimes the imputation seems to be for the wrong year, so look on either side. 
-        # FIXME document wrong imputations. FIXME make this a function.
-        matched = false
-        for year in [fy,fy-1,fy+1] # the fy, the year before, then the year after
-            if v ≈ HISTORIC_BENEFITS[year][:pip_daily_living_enhanced]
-                d[personal_independence_payment_daily_living] = 1
-                matched = true
-                break
-            elseif v ≈ HISTORIC_BENEFITS[year][:pip_daily_living_standard]
-                d[personal_independence_payment_daily_living] = 2
-                matched = true
-                break
-            else
-                println( "personal_independence_payment_daily_living $year $v not matched")
-            end
-        end # for
-        if ! matched # just find the closest - actually probably all we ever need is this
-            d[personal_independence_payment_daily_living] = nearesti( v, 
-                HISTORIC_BENEFITS[fy][:pip_daily_living_standard],
-                HISTORIC_BENEFITS[fy][:pip_daily_living_enhanced] )
-        end
-        @assert d[personal_independence_payment_daily_living] = in [1,2]
-    end # PIP DL
-    matched = false
-    if haskey( incd, :income_personal_independence_payment_mobility )    
-        # note we're testing both fy and previous year here
-        matched = false
-        v = incd[:income_personal_independence_payment_mobility]
-        for year in [fy,fy-1,fy+1]
-            if v ≈ HISTORIC_BENEFITS[year][:pip_mobility_enhanced]
-                d[personal_independence_payment_mobility] = 1
-                matched = true
-                break;
-            elseif v ≈ HISTORIC_BENEFITS[year][:pip_mobility_standard]
-                d[personal_independence_payment_mobility] = 2
-                matched = true
-                break;
-            else
-                println( "personal_independence_payment_mobility $year $v not matched")
-            end
-        end # loop
-        if ! matched
-            # just assign whatever's nearest
-            d[personal_independence_payment_mobility] = nearesti( v, 
-                HISTORIC_BENEFITS[fy][:pip_mobility_standard],
-                HISTORIC_BENEFITS[fy][:pip_mobility_enhanced] )
-        end
-        @assert d[personal_independence_payment_mobility] = in [1,2]
-    end # PIP mob
-    =#
-
 end # module
