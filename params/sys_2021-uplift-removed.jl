8b0ffd4a7ce8bd4e07160aa9284de83d8849b71d
Graham Stark <graham.stark@virtual-worlds.biz>
2021-09-21 10:29:38 +0100

    mini post covid param file

---
+++ params/sys_2021-uplift-removed.jl
@@ -0,0 +1,15 @@
+#
+# changes from 2021/2 files to remove the "20 uplift" from October 2021
+# See:
+# Department for Work and Pensions. 2021. ‘EXPLANATORY MEMORANDUM TO THE UNIVERSAL CREDIT (EXTENSION OF CORONAVIRUS MEASURES) REGULATIONS 2021 2021 No. 313’. https://www.legislation.gov.uk/uksi/2021/313/pdfs/uksiem_20210313_en.pdf.
+#
+
+sys.uc.age_18_24 = 257.33/WEEKS_PER_MONTH
+sys.uc.age_25_and_over = 324.84/WEEKS_PER_MONTH
+sys.uc.couple_both_under_25 = 403.93/WEEKS_PER_MONTH
+sys.uc.couple_oldest_25_plus = 509.91/WEEKS_PER_MONTH
+
+
+sys.lmt.working_tax_credit.basic = 2_005/WEEKS_PER_YEAR
+
+## FIXME JSA/ESA/IS/HB no uplift ??? Think so.
