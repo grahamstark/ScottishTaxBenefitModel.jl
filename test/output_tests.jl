5a8cc3524d54139a3ce0dc3d9ec42c58426a6972
Graham Stark <graham.stark@virtual-worlds.biz>
2021-11-08 14:01:19 +0000

    simple tests of IO

---
+++ test/output_tests.jl
@@ -0,0 +1,29 @@
+using Test
+using CSV
+using DataFrames
+using StatsBase
+using BenchmarkTools
+using PrettyTables
+
+using ScottishTaxBenefitModel
+using ScottishTaxBenefitModel.GeneralTaxComponents
+using ScottishTaxBenefitModel.STBParameters
+using ScottishTaxBenefitModel.RunSettings: Settings, MT_Routing
+using .STBOutput: make_poverty_line, summarise_inc_frame, 
+    dump_frames, summarise_frames, make_gain_lose
+
+settings = Settings()
+
+@testset "basic gain lose" begin
+    pre = DataFrame( income=>[1,2,3], weighted_people=>[1,1,1]);
+    post = DataFrame( income=>[3,2,1], weighted_people=>[1,1,1]);
+    gl = make_gain_lose( pre, post, :income )
+    @test gl.gainers == 1
+    @test gl.losers == 1
+    @test gl.nc == 1
+    @test gl.population == 3
+end
+
+@testset "povery line"
+
+end
