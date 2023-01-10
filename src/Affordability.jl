4137351a64cd2cf038cc00069316b3a1ecdfb672
Graham Stark <graham.stark@virtual-worlds.biz>
2023-01-10 09:11:21 +0000

    Update Affordability.jl

--- src/Affordability.jl
+++ src/Affordability.jl
@@ -1,4 +1,10 @@
 module Affordability
+#
+# start of an Affordability module. See, for example:
+# Stark, G. (2009) Assessing the Ability to Pay for the Fees Charged by Charities (Stage 2 Report). Office of the Scottish Charities Regulator (OSCR). Available at: http://www.oscr.org.uk/publications-and-guidance/affordability-report-phase-2/.
+# Stark, G. (no date) ‘ASSESSING THE ABILITY TO PAY FOR THE FEES CHARGED BY CHARITIES PHASE 1 REPORT’, p. 36.
+#
+#
 
 struct AffordabilityMeasures{T<:Real} 
     share :: T
