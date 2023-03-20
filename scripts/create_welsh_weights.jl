9b6097c48d4d108505850839c673417d5246c581
Graham Stark <graham.stark@virtual-worlds.biz>
2023-03-20 21:01:23 +0000

    wales projections

--- scripts/create_welsh_weights.jl
+++ scripts/create_welsh_weights.jl
@@ -1,17 +1,12 @@
 #
 # 
 #
-
 using ScottishTaxBenefitModel
 using .RunSettings
 using .FRSHouseholdGetter
 using .Weighting
 using .Definitions
 using .ModelHousehold
-
-TARGET_NATION=Wales
-TARGET_YEAR=2023
-
 using DataFrames
 
 include( joinpath(SRC_DIR,"targets","wales-2023.jl"))
@@ -26,6 +21,7 @@ settings.upper_multiple = 4.0
 @time nhhx, num_peoplex, nhh2x = initialise( settings; reset=true )
 
 f = open( "tmp/wales_weights.csv","w")
+println( f, "hid,data_year,weight")
 for hno in 1:nhhx
     mhh = get_household( hno )
     println( f, "$(mhh.hid),$(mhh.data_year),$(mhh.weight)")
