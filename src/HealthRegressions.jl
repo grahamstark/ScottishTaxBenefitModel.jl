656e7778ba2ae6273fc975f6b3b569c2ba1892ff
Graham Stark <graham.stark@virtual-worlds.biz>
2023-07-19 22:47:55 +0100

    Faster, and now right-ish, health regressions

--- src/HealthRegressions.jl
+++ src/HealthRegressions.jl
@@ -8,6 +8,7 @@ using StatsBase
 using Observables
 using ScottishTaxBenefitModel
 using .Definitions
+using .FRSHouseholdGetter: get_regression_dataset
 using .GeneralTaxComponents:WEEKS_PER_MONTH
 using .ModelHousehold
 using .Monitor: Progress
