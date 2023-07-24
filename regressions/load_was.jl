fcf5fe38e0aea26fe324f5e9f1def569a17d7e37
Graham Stark <graham.stark@virtual-worlds.biz>
2023-07-24 23:35:29 +0100

    Wealth wealth wealth.

--- regressions/load_was.jl
+++ regressions/load_was.jl
@@ -15,6 +15,16 @@ was = CSV.File( "/mnt/data/was/UKDA-7215-tab/tab/was_round_7_hhold_eul_march_202
 lcnames = lowercase.(names(was))
 rename!( was, lcnames )
 
+# household reference person only 
+was.employee = was.hrpdvecactr7 .== 1
+was.selfemp = was.hrpdvecactr7 .== 2
+was.unemployed = was.hrpdvecactr7 .== 3  # Unemployed
+was.student = was.hrpdvecactr7 .== 4    # Student
+was.inactive = in.(was.hrpdvecactr7,[[5,9]])    # Looking after family home
+was.sick =  in.(was.hrpdvecactr7, [[6,7]])    # Sick or disabled
+was.retired = was.hrpempstat2r7 .== 8     # Retired
+
+
 was.north_east = was.gorr7 .== 1
 was.north_west = was.gorr7 .== 2
 was.yorkshire = was.gorr7 .== 4
@@ -37,7 +47,7 @@ was.hrp_75_plus = was.hrpdvage8r7 .== 8
 was.weekly_net_income = was.hhnetincmthr7 ./ 4.35
 was.owner = was.ten1r7 .== 1
 was.mortgaged = was.ten1r7 .== 2 .|| was.ten1r7 .== 3
-was.renter = was.ten1r7 .== 4 # should never happen
+was.renter = was.ten1r7 .>= 4 
 was.detatched = (was.accomr7 .== 1) .& (was.hsetyper7 .== 1)
 was.semi = (was.accomr7 .== 1) .& (was.hsetyper7 .== 2)
 was.terraced = (was.accomr7 .== 1) .& (was.hsetyper7 .== 3)
@@ -59,8 +69,14 @@ was.net_housing = was.hpropwr7
 was.net_physical = was.hphyswr7
 was.total_pensions = was.totpenr7_aggr
 was.net_financial = was.hfinwntr7_sum
+was.has_pension_wealth = was.total_pensions .> 0 
 
 was_owner = was[was.ten1r7 .<=3,:]
+was.is_in_debt = was.net_financial .< 0
+
+was = was[was.weekly_net_income .> 0, :] # drop -ive current incomes
+
+
 
 #
 # Count housing wealth as 'physical' if recorded as 'renting'.
--- regressions/wealth_regressions.jl
+++ regressions/wealth_regressions.jl
@@ -13,10 +13,40 @@ summarystats( was.net_physical )
 summarystats( was.net_housing )
 summarystats( was.total_pensions )
 
-reg_net_financial_1 = lm( @formula( net_financial  ~ scotland + wales + london + detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
-           hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + 
-           hrp_u_75 + weekly_net_income + managerial + intermediate + 
-           num_adults + num_children), was )
+reg_is_in_debt_1 = glm( @formula( is_in_debt ~ 
+    scotland + wales + london + 
+    detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
+    owner + mortgaged + 
+    employee + selfemp + unemployed + student + inactive + sick +   
+    hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + weekly_net_income + managerial + intermediate + 
+    num_adults + num_children), was, Binomial(), ProbitLink() )
+
+reg_net_financial_1 = lm( @formula( net_financial  ~ 
+    scotland + wales + london + 
+    detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
+    owner + mortgaged +    
+    employee + selfemp + unemployed + student + inactive + sick +   
+    hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + 
+    weekly_net_income + managerial + intermediate + 
+    num_adults + num_children), was )
+
+reg_net_financial_2 = lm( @formula( log(net_financial) ~ 
+    scotland + wales + london + 
+    detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
+    owner + mortgaged +    
+    employee + selfemp + unemployed + student + inactive + sick +   
+    hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + 
+    log(weekly_net_income) + managerial + intermediate + 
+    num_adults + num_children), was[was.net_financial.>0,:] )
+
+reg_net_financial_3 = lm( @formula( log(-net_financial) ~ 
+    scotland + wales + london + 
+    detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
+    owner + mortgaged +    
+    employee + selfemp + unemployed + student + inactive + sick +   
+    hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + 
+    log(weekly_net_income) + managerial + intermediate + 
+    num_adults + num_children), was[was.net_financial.<0,:] )
 
 reg_net_financial_2 = lm( @formula( net_financial  ~ scotland + wales + london + hbedrmr7 +
            hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + 
@@ -36,17 +66,31 @@ reg_net_financial_4 = lm( @formula( net_financial  ~ scotland + wales + london +
 reg_net_physical_1 = lm( @formula( net_physical  ~ scotland + wales + london + detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
     hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + weekly_net_income + managerial + intermediate + 
     num_adults + num_children), was )
+
 reg_net_physical_2 = lm( @formula( net_physical  ~ scotland + wales + london + detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
     hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + weekly_net_income + managerial + intermediate + 
     num_adults + num_children), was )
 
-reg_total_pensions_1 = lm( @formula( total_pensions  ~ scotland + wales + london + detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
-    hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + weekly_net_income + managerial + intermediate + 
-    num_adults + num_children), was[was.total_pensions.>0,:] )
-
 reg_net_housing_1 = lm( @formula( net_housing ~ scotland + wales + london + detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
     hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + weekly_net_income + managerial + intermediate + 
     num_adults + num_children), was[was.net_housing .>0,:] )
+
+reg_net_housing_2 = lm( @formula( log(net_housing) ~ scotland + wales + london + detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
+    hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + 
+    log(weekly_net_income) + managerial + intermediate + 
+    num_adults + num_children), was[(was.net_housing .>0).&(was.weekly_net_income .>0),:] )
+
+has_pension = glm( @formula( has_pension_wealth ~ scotland + wales + london + detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
+    hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + weekly_net_income + managerial + intermediate + 
+    num_adults + num_children), was, Binomial(), ProbitLink() )
+
+reg_total_pensions_1 = lm( @formula( total_pensions  ~ scotland + wales + london + detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
+    hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + weekly_net_income + managerial + intermediate + 
+    num_adults + num_children), was[was.total_pensions.>0,:] )
+reg_total_pensions_2 = lm( @formula( log(total_pensions)  ~ scotland + wales + london + detatched + semi + terraced + purpose_build_flat + hbedrmr7 +
+    hrp_u_25 + hrp_u_35 + hrp_u_45 + hrp_u_55 + hrp_u_65 + hrp_u_75 + 
+    log(weekly_net_income) + managerial + intermediate + 
+    num_adults + num_children), was[(was.total_pensions.>0).&(was.weekly_net_income .> 0),:] )
 
 regtable( reg_net_financial_1, reg_net_physical_1, reg_total_pensions_1, reg_net_housing_1 ; renderSettings = latexOutput("total_hh_disagg_wealth.tex") )
 regtable( reg_net_financial_1, reg_net_physical_1, reg_total_pensions_1, reg_net_housing_1 )
