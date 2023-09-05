bd6a398d4311d67c33c68f155c9769c58c431343
Graham Stark <graham.stark@virtual-worlds.biz>
2023-09-05 20:41:42 +0100

    Wealth closer to Wealth Tax Commission, exept fixed a HH level.

--- src/STBOutput.jl
+++ src/STBOutput.jl
@@ -682,9 +682,9 @@ const EXTRA_INC_COLS = 18
         ns = Symbol.(colnames)
         select!( sort!(vhh, col), ns... )
         # next one not needed?? 
+        glf."Average Change(£s)" = combine(groupby( dhh, [col]),(:weighted_change=>sum))[:,2]
         glf = coalesce.( vhh, 0.0)
         # add an average change column
-        glf."Average Change(£s)" = combine(groupby( dhh, [col]),(:weighted_change=>sum))[:,2]
         return glf
     end
 
