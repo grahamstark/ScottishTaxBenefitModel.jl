71ba2f405b673f049f07a068552eef53c1c19daa
Graham Stark <graham.stark@virtual-worlds.biz>
2024-06-26 00:09:00 +0100

    takeup crap

---
+++ src/CrudeTakeup.jl
@@ -0,0 +1,12 @@
+module CrudeTakeup
+# crappy https://ifs.org.uk/sites/default/files/output_url_files/ifs-takeup1b.pdf
+
+const UCT_WTC_SINGPAR = 72.0/100.0
+const UCT_WTC_OTHERS = 50.0/100.0
+
+function takeprop( ) :: Real
+
+end
+
+
+end
