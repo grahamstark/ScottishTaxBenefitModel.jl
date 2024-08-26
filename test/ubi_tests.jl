9d4a7d7a828afbc402570b14c938cbbff6c73651
Graham Stark <graham.stark@virtual-worlds.biz>
2024-08-26 10:19:14 +0100

    ub test for conjoint paper start

--- test/ubi_tests.jl
+++ test/ubi_tests.jl
@@ -174,20 +174,34 @@ end
 """
 
 @testset "Conjoint Base Case " begin
-    
-# end function map_features!( tb :: TaxBenefitSystem, facs :: Factors )
-  tb.ubi.abolished = false
-  tb.ubi.mt_bens_treatment = ub_as_is # ub_keep_housing
-      # "Child - £0; Adult - £63; Pensioner - £190"
-      tb.ubi.adult_amount = 63
-      tb.ubi.child_amount = 0
-      tb.ubi.universal_pension = 190
+    settings = get_all_uk_settings_2023()
+    settings.do_marginal_rates = false
+    settings.requested_threads = 4
+    settings.means_tested_routing = uc_full
+    settings.do_health_esimates = true
+    # settings.ineq_income_measure = bhc_net_income # FIXME TEMP
+
+    """
+    load 23/4 
+    """
+    year = 2023
+    scotland = false
+    sys1 = STBParameters.get_default_system_for_fin_year( year, scotland=scotland )
+    sys2 = deepcopy( sys1)
+    # end function map_features!( tb :: TaxBenefitSystem, facs :: Factors )
+    sys2.ubi.abolished = false
+    sys2.ubi.mt_bens_treatment = ub_as_is # ub_keep_housing
+        # "Child - £0; Adult - £63; Pensioner - £190"
+    sys2.ubi.adult_amount = 63
+    sys2.ubi.child_amount = 0
+    sys2.ubi.universal_pension = 190
 
 
-      # "Basic rate - 20%; Higher rate - 40%; Additional rate - 45%"
-      tb.it.non_savings_rates = [0.2, 0.4, 0.45 ]
-      tb.ubi.entitlement = ub_ent_all
-      tb.ubi.income_limit = -1.0
+    # "Basic rate - 20%; Higher rate - 40%; Additional rate - 45%"
+    sys2.it.non_savings_rates = [0.2, 0.4, 0.45 ]
+    sys2.ubi.entitlement = ub_ent_all
+    sys2.ubi.income_limit = -1.0
 
-  make_ubi_pre_adjustments!( tb )
+    make_ubi_pre_adjustments!( sys2 )
+
 end
