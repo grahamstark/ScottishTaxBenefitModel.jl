47a7c33de383b6b4d6a25aa76510f08e8f254b8b
Graham Stark <graham.stark@virtual-worlds.biz>
2025-02-20 06:55:36 +0000

    start shs mapping

---
+++ test/new_style_matching_tests.jl
@@ -0,0 +1,282 @@
+using Test
+
+using CSV,
+    DataFrames,
+    Measures,
+    StatsBase,
+    ArgCheck,
+    PrettyTables
+using ScottishTaxBenefitModel
+using .Definitions, .MatchingLibs, .FRSHouseholdGetter, .RunSettings
+
+import ScottishTaxBenefitModel.MatchingLibs.SHS as shs
+import ScottishTaxBenefitModel.MatchingLibs.LCF as lcf
+import ScottishTaxBenefitModel.MatchingLibs.Common as common
+import ScottishTaxBenefitModel.MatchingLibs.Model as mm
+import ScottishTaxBenefitModel.MatchingLibs.WAS as was
+
+# then ..
+settings = Settings()
+settings.num_households, settings.num_people, nhh = 
+    FRSHouseholdGetter.initialise( settings; reset=false)
+
+@testset "WAS Mappings" begin
+    
+
+
+end
+
+@testset "Model Mappings" begin
+    
+
+end
+
+function map_one!( df :: DataFrame, key :: Symbol, pos :: Vector{Int}; weight=1.0 )
+    l = length(pos)
+    for i in 1:l
+        p = min(pos[i],21) # deliberate miss at end
+        p = p <= 0 ? 21 : p
+        df[p,Symbol("$(key)_$(i)")] += weight
+    end
+end
+
+function map_one!( df :: DataFrame, key :: Symbol, count :: Int; weight=1.0 )
+    if count == 0
+        count = 21
+    end
+    df[count,Symbol("$(key)_1")] += weight
+end
+
+function map_one!( df :: DataFrame, key :: Symbol, count :: Bool; weight=1.0 )
+    p = Int(count)+1 # deliberate miss at end
+    df[p,Symbol("$(key)_1")] += weight
+end
+
+function map_one!( df :: DataFrame, key :: Symbol, count :: Missing; weight=1.0 )
+    p = 21 # deliberate miss at end
+    df[p,Symbol("$(key)_1")] += weight
+end
+
+function to_pct( df:: DataFrame)::DataFrame
+    nr,nc = size(df)
+    for c in 2:nc
+        s = sum(df[!,c])
+        df[!,c] = 100 .* df[!,c] ./ s
+    end
+    df
+end
+
+function one_shs_model_summary_df()::DataFrame
+    n = 21
+    return DataFrame( 
+        shelter_1 = zeros(n),
+        tenure_1 = zeros(n),
+        singlepar_1 = zeros(n),
+        numadults_1 = zeros(n),
+        numkids_1 = zeros(n),
+        acctype_1 = zeros(n),
+        agehigh_1 = zeros(n),
+        empstathigh_1 = zeros(n),
+        ethnichigh_1 = zeros(n),
+        sochigh_1 = zeros(n),
+        datayear_1 = zeros(n),
+        bedrooms_1 = zeros(n),
+        region_1 = zeros(n),
+        shelter_2 = zeros(n),
+        tenure_2 = zeros(n),
+        singlepar_2 = zeros(n),
+        numadults_2 = zeros(n),
+        numkids_2 = zeros(n),
+        acctype_2 = zeros(n),
+        agehigh_2 = zeros(n),
+        empstathigh_2 = zeros(n),
+        ethnichigh_2 = zeros(n),
+        sochigh_2 = zeros(n),
+        datayear_2 = zeros(n),
+        bedrooms_2 = zeros(n),
+        region_2 = zeros(n),
+        shelter_3 = zeros(n),
+        tenure_3 = zeros(n),
+        singlepar_3 = zeros(n),
+        numadults_3 = zeros(n),
+        numkids_3 = zeros(n),
+        acctype_3 = zeros(n),
+        agehigh_3 = zeros(n),
+        empstathigh_3 = zeros(n),
+        ethnichigh_3 = zeros(n),
+        sochigh_3 = zeros(n),
+        datayear_3 = zeros(n),
+        bedrooms_3 = zeros(n),
+        region_3 = zeros(n),
+    )
+end
+
+function one_lcf_model_summary_df()::DataFrame
+    n = 21
+    return DataFrame( 
+        n = 1:n,
+        marstat_1 = zeros(n),
+        socio_1 = zeros(n),
+        empstat_1 = zeros(n),
+        tenure_1 = zeros(n),
+        acctype_1 = zeros(n),
+        region_1 = zeros(n),
+        num_people_1 = zeros(n),
+        num_children_1 = zeros(n),
+        any_disabled_1 = zeros(n),
+        has_female_adult_1 = zeros(n),
+        num_employees_1 = zeros(n),
+        num_pensioners_1 = zeros(n),
+        num_fulltime_1 = zeros(n),
+        num_parttime_1 = zeros(n),
+        num_selfemp_1 = zeros(n),
+        num_unemployed_1 = zeros(n),
+        num_unoccupied_1 = zeros(n),
+        hrp_has_partner_1 = zeros(n),
+        any_wages_1 = zeros(n),
+        any_pension_income_1 = zeros(n),
+        any_selfemp_1 = zeros(n),
+        hrp_unemployed_1 = zeros(n),
+        marstat_2 = zeros(n),
+        socio_2 = zeros(n),
+        empstat_2 = zeros(n),
+        tenure_2 = zeros(n),
+        acctype_2 = zeros(n),
+        region_2 = zeros(n),
+        num_people_2 = zeros(n),
+        any_disabled_2 = zeros(n),
+        has_female_adult_2 = zeros(n),
+        num_employees_2 = zeros(n),
+        num_pensioners_2 = zeros(n),
+        num_fulltime_2 = zeros(n),
+        num_parttime_2 = zeros(n),
+        num_selfemp_2 = zeros(n),
+        num_unemployed_2 = zeros(n),
+        num_unoccupied_2 = zeros(n),
+        hrp_has_partner_2 = zeros(n),
+        any_wages_2 = zeros(n),
+        any_pension_income_2 = zeros(n),
+        any_selfemp_2 = zeros(n),
+        hrp_unemployed_2 = zeros(n),
+        marstat_3 = zeros(n),
+        socio_3 = zeros(n),
+        empstat_3 = zeros(n),
+        tenure_3 = zeros(n),
+        acctype_3 = zeros(n),
+        region_3 = zeros(n),
+        num_people_3 = zeros(n),
+        any_disabled_3 = zeros(n),
+        has_female_adult_3 = zeros(n),
+        num_employees_3 = zeros(n),
+        num_pensioners_3 = zeros(n),
+        num_fulltime_3 = zeros(n),
+        num_parttime_3 = zeros(n),
+        num_selfemp_3 = zeros(n),
+        num_unemployed_3 = zeros(n),
+        num_unoccupied_3 = zeros(n),
+        hrp_has_partner_3 = zeros(n),
+        any_wages_3 = zeros(n),
+        any_pension_income_3 = zeros(n),
+        any_selfemp_3 = zeros(n),
+        hrp_unemployed_3 = zeros(n))
+end
+
+@testset "SHS Mappings" begin
+    shss = shs.create_shs(2018:2022)
+    model_summaries = one_shs_model_summary_df()
+    shs_summaries = one_shs_model_summary_df()
+    map_one!.( (shs_summaries,),  (:shelter,),  shss.accsup1 )
+    map_one!.( (shs_summaries,),  (:tenure,),  shs.tenuremap.(shss.tenure)) 
+    map_one!.( (shs_summaries,),  (:singlepar,),  shss.hhtype_new )
+    map_one!.((shs_summaries,),  (:numadults,),  shs.total_people.(shss.totads, -888, false )) 
+    map_one!.( (shs_summaries,),  (:numkids,),  shs.total_people.(shss.numkids, -887, true )) 
+    map_one!.( (shs_summaries,),  (:acctype,),  shs.accomtype.(shss.hb1, shss.hb2) ) 
+    map_one!.( (shs_summaries,),  (:agehigh,),  shs.age.(shss.hihage )) 
+    map_one!.( (shs_summaries,),  (:empstathigh,),  shs.empstat.(shss.hihecon )) 
+    map_one!.( (shs_summaries,),  (:ethnichigh,),  shs.ethnic.(shss.hih_eth2012 )) 
+    map_one!.( (shs_summaries,),  (:sochigh,),  shs.map_social.(shss.hihsoc )) 
+    map_one!.( (shs_summaries,),  (:datayear,),  shss.datayear .- 2017 ) 
+    map_one!.( (shs_summaries,),  (:bedrooms,),  shs.bedrooms.(shss.hc4 )) 
+    for hno in 1:settings.num_households
+        hh = FRSHouseholdGetter.get_household(hno)
+        cts = mm.counts_for_match( hh )
+        map_one!( model_summaries, :region, mm.map_region( hh.region ))
+        map_one!( model_summaries, :tenure, shs.shs_model_tenure( hh.tenure ))
+        map_one!.( (model_summaries,),(:acctype,),  shs.model_to_shs_accommap(hh.dwelling)) 
+        map_one!( (model_summaries,), (:bedrooms,),  shs.bedrooms( hh.bedrooms )) 
+        for (pno,pers) in hh.people
+             if pers.is_hrp
+                map_one!( model_summaries, :num_adults, cts.num_adults )
+                map_one!( model_summaries, :num_children, cts.num_children )
+                map_one!( model_summaries, :any_disabled, cts.has_disabled_member )
+                map_one!( model_summaries,  :agehigh,  shs.age.(pers.age ))
+                map_one!( model_summaries, :empstathigh, shs.shs_model_empstat(pers.employment_status))
+                map_one!( model_summaries, :ethnichigh, shs.shs_model_ethnic(pers.ethnic_group))
+                map_one!( model_summaries,  :sochigh,  shs.shs_model_map_social( pers.occupational_classification )) 
+            end
+        end # people
+    end # households
+
+
+    pretty_table( to_pct(shs_summaries ))
+    pretty_table( to_pct(model_summaries ))
+end
+
+
+@testset "LCF Mappings" begin
+    model_summaries = one_lcf_model_summary_df()
+    lcf_summaries = one_lcf_model_summary_df()
+    lcfs = lcf.create_subset()
+    map_one!.( (lcf_summaries,), (:acctype,), lcf.map_marital.(lcfs.a006p, lcfs.hrp_has_partner ))
+    map_one!.( (lcf_summaries,), (:marstat,), lcf.map_marital.(lcfs.a006p, lcfs.hrp_has_partner ))
+    map_one!.( (lcf_summaries,), (:socio,), lcf.map_socio.( lcfs.a094 ))
+    map_one!.( (lcf_summaries,), (:empstat,), lcf.map_empstat.( lcfs.a206, lcfs.a005p ))
+    map_one!.( (lcf_summaries,), (:tenure,), lcf.map_tenure.(lcfs.a121 ))
+    map_one!.( (lcf_summaries,), (:region,), lcf.map_region.( lcfs.gorx ))
+    map_one!.(  (lcf_summaries,), (:num_people,), lcfs.num_people )
+    map_one!.(  (lcf_summaries,), (:num_children,), lcfs.num_children )
+    map_one!.( (lcf_summaries,), (:any_disabled,), lcfs.has_disabled_member )
+    map_one!.(  (lcf_summaries,), (:has_female_adult,), lcfs.has_female_adult )
+    map_one!.(  (lcf_summaries,), (:num_employees,), lcfs.num_employees )
+    map_one!.(  (lcf_summaries,), (:num_pensioners,), lcfs.num_pensioners )
+    map_one!.(  (lcf_summaries,), (:num_fulltime,), lcfs.num_fulltime )
+    map_one!.(  (lcf_summaries,), (:num_parttime,), lcfs.num_parttime )
+    map_one!.(  (lcf_summaries,), (:num_selfemp,), lcfs.num_selfemp )
+    map_one!.(  (lcf_summaries,), (:num_unemployed,), lcfs.num_unemployed )
+    map_one!.(  (lcf_summaries,), (:num_unoccupied,), lcfs.num_unoccupied )
+    map_one!.(  (lcf_summaries,), (:hrp_has_partner,), lcfs.hrp_has_partner )
+    map_one!.(  (lcf_summaries,), (:any_wages,), lcfs.any_wages )
+    map_one!.(  (lcf_summaries,), (:any_pension_income,), lcfs.any_pension_income )
+    map_one!.(  (lcf_summaries,), (:any_selfemp,), lcfs.any_selfemp )        
+    for hno in 1:settings.num_households
+        hh = FRSHouseholdGetter.get_household(hno)
+        cts = mm.counts_for_match( hh )
+        map_one!( model_summaries, :region, mm.map_region( hh.region ))
+        map_one!( model_summaries, :tenure, mm.map_tenure( hh.tenure ))
+        for (pno,pers) in hh.people
+            # print( pers.socio_economic_grouping); 
+            if pers.is_hrp
+                map_one!( model_summaries, :socio, lcf.map_socio( pers.socio_economic_grouping, pers.employment_status ))
+                map_one!( model_summaries, :empstat, lcf.map_empstat( pers.employment_status ))           
+                map_one!( model_summaries, :marstat, lcf.map_marital( pers.marital_status ))
+                map_one!( model_summaries, :num_people, cts.num_people )
+                map_one!( model_summaries, :num_children, cts.num_children )
+                map_one!( model_summaries, :any_disabled, cts.has_disabled_member )
+                map_one!(  model_summaries, :has_female_adult, cts.has_female_adult )
+                map_one!(  model_summaries, :num_employees, cts.num_employees )
+                map_one!(  model_summaries, :num_pensioners, cts.num_pensioners )
+                map_one!(  model_summaries, :num_fulltime, cts.num_fulltime )
+                map_one!(  model_summaries, :num_parttime, cts.num_parttime )
+                map_one!(  model_summaries, :num_selfemp, cts.num_selfemp )
+                map_one!(  model_summaries, :num_unemployed, cts.num_unemployed )
+                map_one!(  model_summaries, :num_unoccupied, cts.num_unoccupied )
+                map_one!(  model_summaries, :hrp_has_partner, cts.hrp_has_partner )
+                map_one!(  model_summaries, :any_wages, cts.any_wages )
+                map_one!(  model_summaries, :any_pension_income, cts.any_pension_income )
+                map_one!(  model_summaries, :any_selfemp, cts.any_selfemp )        
+            end
+        end
+    end
+    pretty_table( to_pct(lcf_summaries ))
+    pretty_table( to_pct(model_summaries ))
+end
