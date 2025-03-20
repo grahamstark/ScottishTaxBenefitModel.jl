6225268f4dc08e63a0b4c7432e86ef84364e1446
Graham Stark <graham.stark@virtual-worlds.biz>
2025-03-20 22:11:31 +0000

    putting dla pip in some kind of order.

--- scripts/wrangle-2025-statxplore-files.jl
+++ scripts/wrangle-2025-statxplore-files.jl
@@ -1,25 +1,33 @@
-using XLSX,DataFrames,PrettyTables
+using XLSX,DataFrames,PrettyTables,CSV
+#=
 
+
+
+=#
 tables = [
 # ca-by-nation-18-24-table_2025-03-19_15-40-01.xlsx                    
-"dla-by-age-and-region-18-24-table_2025-03-19_13-45-40.xlsx",
-"dla-by-age-and-region-02-18-table_2025-03-19_15-32-08.xlsx",
-"pip-by-age-and-region-13-19-table_2025-03-19_16-23-07.xlsx",
-"pip-by-age-and-region-19-25-table_2025-03-19_16-04-49.xlsx"
+("dla-by-age-and-region-18-24-table_2025-03-19_13-45-40.xlsx",4,13),
+("dla-by-age-and-region-02-18-table_2025-03-19_15-32-08.xlsx",4,13),
+("pip-by-age-and-region-13-19-table_2025-03-19_16-23-07.xlsx",1,10),
+("pip-by-age-and-region-19-25-table_2025-03-19_16-04-49.xlsx",1,10)
 ]
 
-dir = joinpath( "data", "2025-6")
+dir = joinpath( "/", "mnt","data", "ScotBen", "data", "2025-6")
 
 for tab in tables
-    fname = joinpath( dir, tab )
-    println( "Opening $fname" )
-    d1 = XLSX.readtable( fname, 3, "B:AB"; first_row=11)|>DataFrame
+    infname = joinpath( dir, tab[1] )
+    fn = match(r"(.*)-table.*",tab[1])[1]
+    outfname = joinpath( dir, "$(fn).tab" )
+    startsheet=tab[2]
+    endsheet=tab[3]
+    println( "Opening $infname" )
+    d1 = XLSX.readtable( infname, startsheet, "B:AB"; first_row=11)|>DataFrame
     delete!(d1,1)
     println( "1st sheet data")
     pretty_table( d1 )
     nrows,ncols = size(d1)    
-    for sheet in 4:12
-        d2 = XLSX.readtable( fname, sheet, "B:AB"; first_row=11)|>DataFrame
+    for sheet in (startsheet+1):endsheet
+        d2 = XLSX.readtable( infname, sheet, "B:AB"; first_row=11)|>DataFrame
         delete!(d2,1)
         @assert size(d1) == size(d2)
         for r in 1:nrows, c in 1:ncols
@@ -31,4 +39,5 @@ for tab in tables
     d1 = permutedims(d1,1)
     println( "final table")
     pretty_table(d1)
+    CSV.write( outfname, d1; delim="\t")
 end # tables
