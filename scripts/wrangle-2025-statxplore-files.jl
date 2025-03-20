2d42fe35a733f0672189b1fc70153abc3fc661f0
Graham Stark <graham.stark@virtual-worlds.biz>
2025-03-20 22:40:55 +0000

    putting dla pip in some kind of order.

--- scripts/wrangle-2025-statxplore-files.jl
+++ scripts/wrangle-2025-statxplore-files.jl
@@ -1,15 +1,16 @@
 using XLSX,DataFrames,PrettyTables,CSV
 #=
 
-
+This aggregates all the by year group stuff into an aggregate for 16-65
+year olds. So we can tra
 
 =#
 tables = [
-# ca-by-nation-18-24-table_2025-03-19_15-40-01.xlsx                    
-("dla-by-age-and-region-18-24-table_2025-03-19_13-45-40.xlsx",4,13),
-("dla-by-age-and-region-02-18-table_2025-03-19_15-32-08.xlsx",4,13),
-("pip-by-age-and-region-13-19-table_2025-03-19_16-23-07.xlsx",1,10),
-("pip-by-age-and-region-19-25-table_2025-03-19_16-04-49.xlsx",1,10)
+# fname, startsheet, endsheet, cols
+("dla-by-age-and-region-02-18-table_2025-03-19_15-32-08.xlsx",4,13,"B:BN"),
+("dla-by-age-and-region-18-24-table_2025-03-19_13-45-40.xlsx",4,13,"B:AB"),
+("pip-by-age-and-region-13-19-table_2025-03-19_16-23-07.xlsx",1,10,"B:BS"),
+("pip-by-age-and-region-19-25-table_2025-03-19_16-04-49.xlsx",1,10,"B:BW")
 ]
 
 dir = joinpath( "/", "mnt","data", "ScotBen", "data", "2025-6")
@@ -21,13 +22,13 @@ for tab in tables
     startsheet=tab[2]
     endsheet=tab[3]
     println( "Opening $infname" )
-    d1 = XLSX.readtable( infname, startsheet, "B:AB"; first_row=11)|>DataFrame
+    d1 = XLSX.readtable( infname, startsheet, tab[4]; first_row=11)|>DataFrame
     delete!(d1,1)
     println( "1st sheet data")
     pretty_table( d1 )
     nrows,ncols = size(d1)    
     for sheet in (startsheet+1):endsheet
-        d2 = XLSX.readtable( infname, sheet, "B:AB"; first_row=11)|>DataFrame
+        d2 = XLSX.readtable( infname, sheet, tab[4]; first_row=11)|>DataFrame
         delete!(d2,1)
         @assert size(d1) == size(d2)
         for r in 1:nrows, c in 1:ncols
