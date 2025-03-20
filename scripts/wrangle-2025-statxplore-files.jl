0161b68760bb4cfde92b6e7816e7a91629f1d454
Graham Stark <graham.stark@virtual-worlds.biz>
2025-03-20 21:45:54 +0000

    shs working version

--- scripts/wrangle-2025-statxplore-files.jl
+++ scripts/wrangle-2025-statxplore-files.jl
@@ -1,17 +1,34 @@
-using XLSX,DataFrames
-d1 = XLSX.readtable( "data/2025-6/table_2025-03-19_13-45-40.xlsx", 1, "B:AB"; first_row=11)|>DataFrame
-delete!(d1,1)
-nrows,ncols = size(d1)    
-for i in 2:12
-   d2 = XLSX.readtable( "data/2025-6/table_2025-03-19_13-45-40.xlsx", i, "B:AB"; first_row=11)|>DataFrame
-   delete!(d2,1)
-   @assert size(d1) == size(d2)
-   for r in 1:nrows, c in 1:ncols
-      if (typeof( d2[r,c] )<:Number) && (typeof(d1[r,c])<:Number)
-         d1[r,c] += d2[r,c]
-      end
-   end
-end
+using XLSX,DataFrames,PrettyTables
 
-d1 = permutedims(d1,1)
-d1
+tables = [
+# ca-by-nation-18-24-table_2025-03-19_15-40-01.xlsx                    
+"dla-by-age-and-region-18-24-table_2025-03-19_13-45-40.xlsx",
+"dla-by-age-and-region-02-18-table_2025-03-19_15-32-08.xlsx",
+"pip-by-age-and-region-13-19-table_2025-03-19_16-23-07.xlsx",
+"pip-by-age-and-region-19-25-table_2025-03-19_16-04-49.xlsx"
+]
+
+dir = joinpath( "data", "2025-6")
+
+for tab in tables
+    fname = joinpath( dir, tab )
+    println( "Opening $fname" )
+    d1 = XLSX.readtable( fname, 3, "B:AB"; first_row=11)|>DataFrame
+    delete!(d1,1)
+    println( "1st sheet data")
+    pretty_table( d1 )
+    nrows,ncols = size(d1)    
+    for sheet in 4:12
+        d2 = XLSX.readtable( fname, sheet, "B:AB"; first_row=11)|>DataFrame
+        delete!(d2,1)
+        @assert size(d1) == size(d2)
+        for r in 1:nrows, c in 1:ncols
+            if (typeof( d2[r,c] )<:Number) && (typeof(d1[r,c])<:Number)
+                d1[r,c] += d2[r,c]
+            end
+        end
+    end # sheets
+    d1 = permutedims(d1,1)
+    println( "final table")
+    pretty_table(d1)
+end # tables
