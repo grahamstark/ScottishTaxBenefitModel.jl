8dd6cfa673f1d2d80add1ae67cfb2140192fa17e
Graham Stark <graham.stark@virtual-worlds.biz>
2025-04-08 00:09:13 +0100

    2025/6 system start

--- params/sys_2025_26_scotland.jl
+++ params/sys_2025_26_scotland.jl
@@ -1,5 +1,5 @@
 """
- SCOTLAND 2024/5
+ SCOTLAND 2025/6
 
  Sources:
  IT: https://www.gov.scot/publications/scottish-income-tax-2024-25-factsheet/
@@ -7,7 +7,7 @@
  BRMA: https://www.gov.scot/publications/local-housing-allowance-rates-2024-2025/
  Benefits: https://bprcdn.parliament.scot/published/2024/3/14/6f58227d-21aa-4016-91e6-f3d0bb29962d/SB%2024-15.pdf
 """
-function load_sys_2024_25_scotland!( sys :: TaxBenefitSystem{T} ) where T
+function load_sys_2025_26_scotland!( sys :: TaxBenefitSystem{T} ) where T
     sys.name = "Scottish System 2024/25"
     sys.it.non_savings_rates = T[
         19.0, # starter
@@ -29,39 +29,39 @@ function load_sys_2024_25_scotland!( sys :: TaxBenefitSystem{T} ) where T
     ## !!! FIXME the Welsh ones in this file are not updated 
     brmapath = joinpath( artifact"augdata", "lha_rates_scotland_2024_25.csv")
     sys.hr.brmas = loadBRMAs( 4, T, brmapath )
-    sys.loctax.ct.band_d = Dict(
-        [:S12000033 => 1_489.55, # Aberdeen City
-        :S12000034 => 1_393.42, # Aberdeenshire
-        :S12000041 => 1_316.68, # Angus
-        :S12000035 => 1_627.12, # Argyll and Bute
-        :S12000036 => 1_447.69, # City of Edinburgh
-        :S12000005 => 1_410.96, # Clackmannanshire
-        :S12000006 => 1_334.85, # Dumfries and Galloway
-        :S12000042 => 1_486.43, # Dundee City
-        :S12000008 => 1_487.44, # East Ayrshire
-        :S12000045 => 1_415.66, # East Dunbartonshire
-        :S12000010 => 1_435.62, # East Lothian
-        :S12000011 => 1_415.22, # East Renfrewshire
-        :S12000014 => 1_363.82, # Falkirk
-        :S12000047 => 1_385.18, # Fife
-        :S12000049 => 1_499.00, # Glasgow City
-        :S12000017 => 1_427.19, # Highland
-        :S12000018 => 1_547.01, # Inverclyde
-        :S12000019 => 1_514.73, # Midlothian
-        :S12000020 => 1_430.69, # Moray
-        :S12000013 => 1_290.75, # Na h-Eileanan Siar
-        :S12000021 => 1_452.12, # North Ayrshire
-        :S12000050 => 1_320.78, # North Lanarkshire
-        :S12000023 => 1_369.21, # Orkney Islands
-        :S12000048 => 1_403.69, # Perth and Kinross
-        :S12000038 => 1_436.17, # Renfrewshire
-        :S12000026 => 1_356.11, # Scottish Borders
-        :S12000027 => 1_260.61, # Shetland Islands
-        :S12000028 => 1_453.16, # South Ayrshire
-        :S12000029 => 1_300.81, # South Lanarkshire
-        :S12000030 => 1_481.50, # Stirling
-        :S12000039 => 1_398.98, # West Dunbartonshire
-        :S12000040 => 1_390.96]) # West Lothian
+    sys.loctax.ct.band_d = Dict([
+        :S12000033 => 	1636.27		, # Aberdeen City
+        :S12000034 => 	1532.76		, # Aberdeenshire
+        :S12000041 => 	1461.52		, # Angus
+        :S12000035 => 	1625.64		, # Argyll and Bute
+        :S12000036 => 	1563.51		, # City of Edinburgh
+        :S12000005 => 	1594.38		, # Clackmannanshire
+        :S12000006 => 	1454.98		, # Dumfries and Galloway
+        :S12000042 => 	1605.34		, # Dundee City
+        :S12000008 => 	1606.44		, # East Ayrshire
+        :S12000045 => 	1599.70		, # East Dunbartonshire
+        :S12000010 => 	1579.18		, # East Lothian
+        :S12000011 => 	1528.44		, # East Renfrewshire
+        :S12000014 => 	1576.77		, # Falkirk
+        :S12000047 => 	1498.76		, # Fife
+        :S12000049 => 	1611.00		, # Glasgow City
+        :S12000017 => 	1527.09		, # Highland
+        :S12000018 => 	1551.30		, # Inverclyde
+        :S12000019 => 	1666.20		, # Midlothian
+        :S12000020 => 	1573.76		, # Moray
+        :S12000013 => 	1387.56		, # Na h-Eileanan Siar
+        :S12000021 => 	1553.77		, # North Ayrshire
+        :S12000050 => 	1452.86		, # North Lanarkshire
+        :S12000023 => 	1574.60		, # Orkney Islands
+        :S12000048 => 	1537.04		, # Perth and Kinross
+        :S12000038 => 	1572.61		, # Renfrewshire
+        :S12000026 => 	1491.72		, # Scottish Borders
+        :S12000027 => 	1386.67		, # Shetland Islands
+        :S12000028 => 	1569.41		, # South Ayrshire
+        :S12000029 => 	1378.85		, # South Lanarkshire
+        :S12000030 => 	1611.78		, # Stirling
+        :S12000039 => 	1559.86		, # West Dunbartonshire
+        :S12000040 	=> 1515.45	])	# West Lothian )
 
     # here so it's always on 
     sys.scottish_child_payment.abolished = false
