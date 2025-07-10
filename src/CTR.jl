dc114b8bb45af19c0e3fcf491f3b0b83e489116d
Graham Stark <graham.stark@virtual-worlds.biz>
2025-07-10 09:15:38 +0100

    Multiple changes, inc start of CRT module

---
+++ src/CTR.jl
@@ -0,0 +1,48 @@
+module CTR
+ 
+using ScottishTaxBenefitModel
+
+using .Intermediate
+using .LegacyMeansTestedBenefits
+using .ModelHousehold
+using .Results
+using .STBParameters
+using .UniversalCredit
+
+export calc_ctr!
+
+function calc_ctr!( 
+    household_result :: HouseholdResult,
+    household        :: Household,
+    intermed         :: HHIntermed,
+    ucsys            :: UniversalCreditSys,
+    ctrsys           :: CTRSys,
+    age_limits       :: AgeLimits, 
+    hours_limits     :: HoursLimits,
+    child_limits     :: ChildLimits )
+    hhr = household_result
+    hh = household # shortcuts
+    if ctrsys.abolished
+        return 
+    end
+    bu = get_benefit_units(household)[1]
+    head = get_head( bu )
+    recipient = head.pid
+    bur = household_result.bus[1] 
+    passported =  has_any( bur, ctrsys.passported_bens... )
+    ct = total( household_result, LOCAL_TAXES )
+    if ! passported         
+        ucincome =  
+            bur.uc.earnings_before_allowances + # gross earned income back up
+            bur.uc.other_income +
+            bur.uc.tariff_income +
+            ucrec
+        excess = max(0.0, ucincome - bur.ctr.maximum)
+        if excess > 0
+            ct = max( 0.0, ct - excess*ctrsys.taper )  
+        end
+        bur.uc.ctr = ct
+    end
+end
+
+end # module
