b0ef5ceed2ca0be6131c7be5ecd7fca4dbb498b9
Juanpe <j.p.castrobaeza@gmail.com>
2025-11-19 17:12:16 +0000

    Added the skeleton module and test file. Included the module in ScottishTaxBenefitModel.jl

---
+++ test/behavioural_tests.jl
@@ -0,0 +1,7 @@
+# This is a placeholder file for behavioural tests.
+
+# Do we need an external 'correct results' to run the tests against?
+# The SFC workbook does this at the aggregate level, but the code runs on individuals
+# and then aggregates. Plus the aggregates would not be the same.
+# We could recreate the worbook but with up to date rates, thresholds, and for a single 
+# person in each band.
