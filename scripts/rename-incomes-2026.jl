c44d8d8aafd7cb2eaf3c6f21f9dfb5402279fe57
Graham Stark <graham.stark@virtual-worlds.biz>
2026-09-02 09:06:23 +0100

    Income Definition stuff.

--- scripts/rename-incomes-2026.jl
+++ scripts/rename-incomes-2026.jl
@@ -1,7 +1,91 @@
+using DataStructures
 using ScottishTaxBenefitModel
-
 using .Definitions
 using .STBIncomes
+
+
+@enum Benefit_Type_ED begin  # mapped from benefit
+   missing_benefit_type = -1
+   dlaself_care = 1
+   dlamobility = 2
+   child_benefit = 3
+   pension_credit = 4
+   state_pension = 5
+   bereavement_allowance_or_widowed_parents_allowance_or_bereavement = 6
+   armed_forces_compensation_scheme = 8
+   war_widows_or_widowers_pension = 9
+   severe_disability_allowance = 10
+   attendance_allowance = 12
+   carers_allowance = 13
+   jobseekers_allowance = 14
+   industrial_injury_disablement_benefit = 15
+   employment_and_support_allowance = 16
+   incapacity_benefit = 17
+   income_support = 19
+   maternity_allowance = 21
+   maternity_grant_from_social_fund = 22
+   funeral_grant_from_social_fund = 24
+   any_other_ni_or_state_benefit = 30
+   trade_union_sick_or_strike_pay = 31
+   friendly_society_benefits = 32
+   private_sickness_scheme_benefits = 33
+   accident_insurance_scheme_benefits = 34
+   hospital_savings_scheme_benefits = 35
+   government_training_allowances = 36
+   guardians_allowance = 37
+   widows_payment = 60
+   unemployment_or_redundancy_insurance = 61
+   winter_fuel_payments = 62
+   child_winter_heating_assistance_payment = 63
+   dwp_third_party_payments_is_or_pc = 65
+   dwp_third_party_payments_jsa_or_esa = 66
+   social_fund_loan_repayment_from_is_or_pc = 69
+   social_fund_loan_repayment_from_jsa_or_esa = 70
+   extended_hb = 78
+   permanent_health_insurance = 81
+   any_other_sickness_insurance = 82
+   critical_illness_cover = 83
+   working_tax_credit = 90
+   child_tax_credit = 91
+   working_tax_credit_lump_sum = 92
+   child_tax_credit_lump_sum = 93
+   housing_benefit = 94
+   universal_credit = 95
+   personal_independence_payment_daily_living = 96
+   personal_independence_payment_mobility = 97
+   a_loan_from_the_dwp_and_dfc = 98
+   a_loan_or_grant_from_local_authority = 99
+   future_pension_credit = 102
+   future_universal_credit = 103
+   future_housing_benefit = 104
+   future_working_tax_credit = 105
+   future_child_tax_credit = 106
+   future_income_support = 107
+   future_jobseekers_allowance = 108
+   future_employment_and_support_allowance = 109
+   dwp_third_party_payments_uc = 110
+   social_fund_loan_uc = 111
+   dwp_third_party_payments_v2 = 1112 # 2019- no idea what this is
+   repayment_uc_advance = 113 # 2019-
+   advance_of_uc = 114 # 2019 -
+   scottish_child_payment = 112
+   job_start_payment = 115
+   troubles_permanent_disablement = 116
+
+   adp_daily_living = 117
+   adp_mobility = 118
+   one_off_irb_payment = 124
+
+   child_disability_payment_care = 121
+   child_disability_payment_mobility = 122
+   pupil_development_grant = 123
+   disability_topup = 125 # 2022 only so far
+   pension_topup = 126  # 2022 only so far
+   carers_allowance_supplement = 999
+   carers_support_payment = 997 # fixme not yet in data - numbers wrong
+   pension_age_disability = 998 # fixme not yet in data - numbers wrong
+end
+
 
 s1 = SortedSet{String}()
 s2 = SortedSet{String}()
@@ -18,21 +102,20 @@ renames = intersect( s1, s2 )
 missing_in_old = setdiff( s2, s1 )
 missing_in_new = setdiff( s1, s2 )
 
-open( "income-renames.sed", "w") do io
+open( "income-renames2.sed", "w") do io
     for i in renames
         println( io, "1,\$s/$i/$(lowercase(i))/g")
     end
 
     println( io, "missing in old")
     for i in missing_in_old
-        println( io, "$i")
+        println( io, "$(lowercase(i))")
     end
 
     println( io, "missing in new")
     for i in missing_in_new
         println( io, "$i")
     end
-
 end
 
 #=
